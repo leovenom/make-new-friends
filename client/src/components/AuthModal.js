9d4fec5844b6c703d8f1f78242603d30d9d20cfe
leovenom <leonardt@outlook.com>
2023-05-15 16:44:05 +0100

    add more

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,12 +1,15 @@
 import { useState } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { useCookies } from "react-cookie";
 
 const AuthModal = ({ setShowModal, isSignUp }) => {
   const [email, setEmail] = useState(null);
   const [password, setPassword] = useState(null);
   const [confirmPassword, setConfirmPassword] = useState(null);
   const [error, setError] = useState(null);
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
+
   let navigate = useNavigate();
 
   console.log(email, password, confirmPassword);
@@ -23,11 +26,14 @@ const AuthModal = ({ setShowModal, isSignUp }) => {
         setError("Passwords need to match!");
         return;
       }
+      console.log("posting", email, password);
 
       const response = await axios.post(
         `http://localhost:8000/${isSignUp ? "signup" : "login"}`,
         { email, password }
       );
+      setCookie("AuthToken", response.data.token);
+      setCookie("UserId", response.data.userId);
 
       const success = response.status === 201;
       if (success && isSignUp) navigate("/onboarding");
--- client/src/pages/OnBoarding.js
+++ client/src/pages/OnBoarding.js
@@ -1,9 +1,13 @@
 import { useState } from "react";
 import Nav from "../components/Nav";
+import { useCookies } from "react-cookie";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const OnBoarding = () => {
+  const [cookies, setCookie, removeCookie] = useCookies(["user"]);
   const [formData, setFormData] = useState({
-    user_id: "",
+    user_id: cookies.UserId,
     first_name: "",
     dob_day: "",
     dob_month: "",
@@ -17,8 +21,19 @@ const OnBoarding = () => {
     matches: [],
   });
 
-  const handleSubmit = (e) => {
-    console.log("submitted");
+  let navigate = useNavigate();
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await axios.put("http://localhost:8000/user", {
+        formData,
+      });
+      const success = response.status === 200;
+      if (success) navigate("/dashboard");
+    } catch (err) {
+      console.log(err);
+    }
   };
 
   const handleChange = (e) => {
@@ -188,7 +203,9 @@ const OnBoarding = () => {
               required={true}
             />
             <div className="photo-container">
-              <img src={formData.url} alt="profile pic preview" />
+              {formData.url && (
+                <img src={formData.url} alt="profile pic preview" />
+              )}
             </div>
           </section>
         </form>
